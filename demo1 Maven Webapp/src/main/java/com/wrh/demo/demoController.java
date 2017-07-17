e37be7a8e8c59c7e0faf2c1d433f21f99764cff5
yitianliangjiao <1945623989@qq.com>
2017-07-17 17:51:49 +0800

    1

---
+++ demo1 Maven Webapp/src/main/java/com/wrh/demo/demoController.java
@@ -0,0 +1,14 @@
+package com.wrh.demo;
+
+import com.sun.corba.se.spi.orbutil.fsm.Guard.Result;
+
+public class demoController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char[] c = new char[]{'0','1','s','d'};
+String s = new String(c, 1, 2);
+System.out.println(s);
+	}
+
+}
